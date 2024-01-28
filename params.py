9d090f5fcbdada7e5d905c12fb48805d32d1f1c3
Matthew Zak <zak.matthew@yahoo.com>
2024-01-28 17:26:49 +0100

    add label transforming dictionaries

---
+++ params.py
@@ -0,0 +1,5 @@
+WANDB_PROJECT = 'fanthom_challenge'
+ENTITY = None # set this to team name if working in a team
+RAW_DATA_AT = 'fanthom_data_raw'
+PROCESSED_DATA_AT = 'fanthom_data_processed'
+FINAL_DATA_AT = 'fanthom_final_data'
